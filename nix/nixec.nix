8003fcedce77a77db310227085e4bf8e28fbb30d
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2020-04-30 11:55:56 +0200

    Some nix stuff

---
+++ nix/nixec.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import fix/nixpkgs.nix {}
+, compiler ? "default"
+, haskellPackages ? 
+    if compiler == "default" 
+    then pkgs.haskellPackages 
+    else pkgs.haskell.packages."${compiler}"
+, dirtree ? import fix/dirtree.nix
+}: 
+haskellPackages.developPackage {
+  root = pkgs.lib.cleanSourceWith 
+    { filter = path: type: baseNameOf path != ".nix";
+      src = pkgs.lib.cleanSource ../.;
+    };
+  name = "nixec";
+  source-overrides = {
+    inherit dirtree;
+  };
+  modifier = drv:
+    with pkgs.haskell.lib;
+    addBuildTools drv (with haskellPackages; [ cabal-install ghcid ]);
+}
